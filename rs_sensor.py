8e9735de39c3711a893df0fb313a1f47df60c5d1
Hyunwoo <woo980711@gmail.com>
2025-02-17 10:23:41 +0900

    [fix] Able to connect several realsense devices

--- rs_sensor.py
+++ rs_sensor.py
@@ -3,11 +3,21 @@ import numpy as np
 
 
 class RSSensor:
-    def __init__(self):
-        """Initialize RealSense sensor."""
+    def __init__(self, sensor_index=0):
+        """Initialize RealSense sensor with a specific serial number."""
         self.pipeline = rs.pipeline()
         self.config = rs.config()
         self.intr_params = None
+        self.serial_number = self._get_connected_devices()[sensor_index]
+        self.config.enable_device(self.serial_number)
+
+    @staticmethod
+    def _get_connected_devices():
+        """Retrieve all connected RealSense devices and return their serial numbers."""
+        context = rs.context()
+        devices = context.query_devices()
+        serial_numbers = [dev.get_info(rs.camera_info.serial_number) for dev in devices]
+        return serial_numbers  # Return Serial Number list for all connected cameras
 
     def get_device_sn(self):
         """Retrieve and print the device serial number."""
@@ -23,8 +33,10 @@ class RSSensor:
         self.config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
         self.config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
         profile = self.pipeline.start(self.config)
+
         color_stream = profile.get_stream(rs.stream.color).as_video_stream_profile()
         self.intr_params = color_stream.get_intrinsics()
+        print(f"Camera {self.serial_number} started.")
 
     def get_data(self):
         """Retrieve RGB and depth data from the sensor."""
@@ -42,3 +54,16 @@ class RSSensor:
     def stop(self):
         """Stop the sensor."""
         self.pipeline.stop()
+        print(f"Camera {self.serial_number} stopped.")
+
+    def get_camera_parameters(self):
+        """Get camera parameters."""
+        if self.intr_params is None:
+            raise RuntimeError("Intrinsic parameters are not available. Start the camera first.")
+
+        ppx, ppy, fx, fy = self.intr_params.ppx, self.intr_params.ppy, self.intr_params.fx, self.intr_params.fy
+        camera_matrix = np.array([[fx, 0, ppx],
+                                  [0, fy, ppy],
+                                  [0, 0, 1]])
+        camera_params = (camera_matrix[0, 0], camera_matrix[1, 1], camera_matrix[0, 2], camera_matrix[1, 2])
+        return camera_matrix, camera_params
